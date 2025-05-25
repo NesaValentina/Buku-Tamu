9f9b25d6f227a00c96d4d9d07e5928f025106b7d
NesaValentina <tentanglangit914@gmail.com>
2025-05-25 18:40:12 +0700

    Add files via upload

---
+++ db_bukutamu.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Bulan Mei 2025 pada 13.38
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bukutamu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_tamu`
+--
+
+CREATE TABLE `buku_tamu` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `instansi` varchar(100) DEFAULT NULL,
+  `tujuan` text DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `waktu` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`id`, `nama`, `instansi`, `tujuan`, `tanggal`, `waktu`) VALUES
+(1, 'Nesa Valentina', 'Universitas Siber Asia', 'Kegiatan Perkuliahan', '2025-05-25', '11:57:49'),
+(13, 'Ahmad Junaedi', 'Universitas Siber Asia', 'Kegiatan Mengajar', '2025-05-25', '12:15:19'),
+(15, 'Tovu Yovi D', 'Universitas Siber Asia', 'Kegiatan Perkuliahan', '2025-05-25', '12:51:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
